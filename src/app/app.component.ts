9317cbd2e1247479faa0d26a4968e119e667b46a
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2025-01-30 13:48:15 +0800

    Init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderCompComponent } from './header-comp/header-comp.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderCompComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   title = 'kal-portfolio';
 }
---
+++ src/app/header-comp/header-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-comp',
+  imports: [],
+  templateUrl: './header-comp.component.html',
+  styleUrl: './header-comp.component.css'
+})
+export class HeaderCompComponent {
+
+}
