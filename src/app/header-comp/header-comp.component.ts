8419d78dfe14032427cd83f6a6c0444021c1530e
Muhammad Haikal <98247943+haikaaal21@users.noreply.github.com>
2025-01-31 11:31:13 +0800

    Finalized header

--- src/app/header-comp/header-comp.component.ts
+++ src/app/header-comp/header-comp.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-header-comp',
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, CommonModule, MatMenuModule],
   templateUrl: './header-comp.component.html',
   styleUrls: ['./header-comp.component.css']
 })
 export class HeaderCompComponent implements OnInit {
   kalTime: string | any;
+  burger: boolean = false;
 
   ngOnInit() {
   }
-  
+
+  openBurgerBar() {
+    this.burger = !this.burger;
+  }
+
   refreshSite() {
     window.location.reload();
+  }
+
+  navigateTo(path: string) {
+    window.location.href = path;
   }
 }
